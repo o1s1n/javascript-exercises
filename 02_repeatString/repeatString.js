9a93e373675d7b0ca57362dbf8a0fb93ee1635eb
o1s1n <oisin@oisinbourke.com>
2022-03-24 00:13:00 +0000

    exercise 1-3 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
 
-};
+const repeatString = function(myString, numOfTimes) 
+{
+    let result = "";
+
+    if(numOfTimes >= 0)
+    {
+        for(i = 1; i <= numOfTimes; i++)
+        {
+            result += myString;
+        }
+    }
+    else
+    {
+        return 'ERROR';
+    }
+    
+    return result;
+
+}
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
-};
+const reverseString = function(myString) 
+{
+    let splitString = myString.split("");
+    let result = "";
+    
+    for (i = splitString.length -1; i >= 0; i--)
+    {
+        result += splitString[i];
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
