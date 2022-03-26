f63f9a48c9feabc533d5f6dab2f46e5ca1829044
o1s1n <oisin@oisinbourke.com>
2022-03-26 18:31:37 +0000

    finally solved the removeFromArray lesson with a filter and a rest parameter

---
+++ 04_removeFromArray/JSnotes.js
@@ -0,0 +1,8 @@
+for (let i = 1; i < array.length; i++) 
+        {
+            result = array[i];
+            for (let j = 0; j < array.length; j++) {
+                const result2 = array[j];
+                
+            }
+        } 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...myNums)
+{
+    
+    // // This passes the first test.
+    // // This method mutates the original array (which is why we return that array).
+    // let arraySplice = myArray.splice(myArray.indexOf(extraNum), 1);
 
-};
+    // console.log("the result = " + myArray);
+    // return myArray;
+
+    const filteredArray = myArray.filter(n => !myNums.includes(n));
+    return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
