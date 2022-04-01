cec2b2a288c2f653b17170c37bb2ecc5c36b975b
o1s1n <oisin@oisinbourke.com>
2022-04-01 20:59:42 +0100

    some attempts at leapyear, a bit sick at the moment. Will try again with clear head.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function(myYear) 
-{
-    ((myYear % 4 == 0) && (myYear % 400 !== 0) && (myYear % 100 !== 0)) ? true : false;
+const leapYears = function(lYear) 
+{       
+    let result;
+    if(lYear % 4 == 0)
+    {
+        if(lYear % 100 == 0)
+        {
+            if((lYear % 400 == 0) && (lYear % 100 == 0))
+            {            
+                result = true;                    
+            }       
+            else
+            {
+                result = false;
+            }
+        }
+        
+    }
+    else
+    {
+        result = false;
+    }
+    //console.log(result);
+    return result
 }
+leapYears(1900);
 // Do not edit below this line
 module.exports = leapYears;
